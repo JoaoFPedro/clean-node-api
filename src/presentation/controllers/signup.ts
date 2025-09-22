9975384d5febe8d700e6b7873dbbcd26b40c5cb4
Joao Pedro <joao.fpsilva98@gmail.com>
2025-09-22 19:30:16 -0300

    feat: adding validation for password and password confirmation

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,15 @@
 import { MissingParamError } from "../erros/missing-params-erros";
 import { HttpRequest, HttpResponse } from "../protocols/https";
 import { badRequest } from "../helpers/http-helper";
-export class SignUpController {
+import { Controller } from "../protocols/controller";
+export class SignUpController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ["name", "email"];
+    const requiredFields = [
+      "name",
+      "email",
+      "password",
+      "confirmationPassword",
+    ];
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
