1f6753ec566de8622aa3e51c0a9015f43af16a73
Joao Pedro <joao.fpsilva98@gmail.com>
2025-10-07 20:40:04 -0300

    feat: ensure presentation throws error when encrypter throws

--- src/data/use-cases/add-account/db-add-account.spec.ts
+++ src/data/use-cases/add-account/db-add-account.spec.ts
@@ -35,4 +35,19 @@ describe("DBAddAccount Use case", () => {
     await sut.add(accountData);
     expect(encryptSpy).toHaveBeenLastCalledWith("any_password");
   });
+  test("Should throw if Encrypter throws", async () => {
+    const { sut, encrypterStub } = makeSut();
+    jest
+      .spyOn(encrypterStub, "encrypt")
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+    const accountData = {
+      name: "any_name",
+      email: "any_email",
+      password: "any_password",
+    };
+    const promise = sut.add(accountData);
+    await expect(promise).rejects.toThrow();
+  });
 });
