7f0a9ecaaefb0f6feb579c930066f7170eb7f365
Joao Pedro <joao.fpsilva98@gmail.com>
2025-10-15 19:09:57 -0300

    feat: ensure bcrypt return hash on sucess

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,10 +1,23 @@
 import bcrypt from "bcrypt";
 import { BCrypter } from "./bcrypt";
+
+jest.mock("bcrypt", () => ({
+  async hash(): Promise<String> {
+    return new Promise((resolve) => resolve("hash"));
+  },
+}));
 describe("Bcrypt Adapter", () => {
   test("Should call bcrypt with correct value", async () => {
     const sut = new BCrypter(12);
     const encryptSpy = jest.spyOn(bcrypt, "hash");
     await sut.encrypt("any_value");
     expect(encryptSpy).toHaveBeenCalledWith("any_value", 12);
+  });
+  test("Should a hash on success case", async () => {
+    const sut = new BCrypter(12);
+    const encryptSpy = jest.spyOn(bcrypt, "hash");
+    const hash = await sut.encrypt("any_value");
+    console.log(hash);
+    expect(hash).toBe("hash");
   });
 });
