b9dec59df2ad77b0497123a2a7d79ab63cbbedda
Joao Pedro <joao.fpsilva98@gmail.com>
2025-12-04 21:02:38 -0300

    .

--- src/data/protocols/add-account-repository.ts
+++
@@ -1,8 +0,0 @@
-import {
-  AccountModel,
-  AddAccountModel,
-} from "../use-cases/add-account/db-add-account-protocols";
-
-export interface AddAccountRepository {
-  add(account: AddAccountModel): Promise<AccountModel | null>;
-}
