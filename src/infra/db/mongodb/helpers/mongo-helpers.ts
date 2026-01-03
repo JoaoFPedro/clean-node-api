04427a957cbedb3d5b902c2d76d4f02c4af03416
Joao Pedro <joao.fpsilva98@gmail.com>
2026-01-03 10:43:55 -0300

    .

--- src/infra/db/mongodb/helpers/mongo-helpers.ts
+++ src/infra/db/mongodb/helpers/mongo-helpers.ts
@@ -1,36 +1,22 @@
 import { MongoClient } from "mongodb";
-import { MongoMemoryServer } from "mongodb-memory-server";
 
 export const MongoHelper = {
   client: null as MongoClient | null,
   uri: null as string | null,
-  memoryServer: null as MongoMemoryServer | null,
 
-  async connect(uri?: string): Promise<void> {
-    // Se não for passada uma URI, inicia o Mongo em memória
-    if (!uri) {
-      this.memoryServer = await MongoMemoryServer.create();
-      this.uri = this.memoryServer.getUri();
-      console.log("🚀 MongoDB em memória iniciado:", this.uri);
-    } else {
-      this.uri = uri;
-    }
-
+  async connect(uri: string): Promise<void> {
+    this.uri = uri;
     this.client = await MongoClient.connect(this.uri);
   },
 
   async disconnect(): Promise<void> {
     await this.client?.close();
     this.client = null;
-    if (this.memoryServer) {
-      await this.memoryServer.stop();
-      console.log("🧹 MongoDB em memória parado");
-    }
   },
 
   async getCollection(name: string) {
     if (!this.client) {
-      await this.connect(this.uri || undefined);
+      await this.connect(this.uri || "");
     }
     return this.client?.db()?.collection(name);
   },
