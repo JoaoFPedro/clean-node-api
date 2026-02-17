003521683b94b3d89cdb80e373fd7cb788bfaa64
Joao Pedro <joao.fpsilva98@gmail.com>
2026-02-17 20:49:47 -0300

    resumed

---
+++ src/main/factories/controllers/login/login/login-factory.ts
@@ -0,0 +1,15 @@
+import { LoginController } from "../../../../../presentation/controllers/login/login/login-controller";
+import { Controller } from "../../../../../presentation/protocols/controller";
+import { makeLoginValidation } from "./login-validation";
+
+import { makeDbAuthentication } from "../../../usecases/authentication/db-authentication";
+import { makeLogControllerDecorator } from "../../../decoratos/log-controller-decotaror-factory";
+
+export const makeLoginController = (): Controller => {
+  const controller = new LoginController(
+    makeLoginValidation(),
+    makeDbAuthentication(),
+  );
+
+  return makeLogControllerDecorator(controller);
+};
